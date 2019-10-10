a54b11c7a9969f907dcbfc6c3981b9becfeb4fde
Piotr Synowiec <psynowiec@gmail.com>
2019-10-10 12:53:06 +0200

    #master: m:d:l --zero (do not display entities with 0 record count)

--- src/Command/DbList.php
+++ src/Command/DbList.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Schema\Table;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DbList extends Command
@@ -15,6 +16,8 @@ class DbList extends Command
     public const COUNT_LENGTH = 10;
     public const LINE_FORMAT = '| %-' . self::ENTITY_LENGTH . 's | %' . self::COUNT_LENGTH . 's |';
     public const LINE_HORIZONTAL_LENGTH = 117;
+
+    private const ZERO_PARAM_NAME = 'zero';
 
     /** @var EntityManagerInterface */
     private $em;
@@ -29,17 +32,26 @@ class DbList extends Command
     {
         $this
             ->setName('mysiar:db:list')
-            ->setDescription('List all entities and records count');
+            ->setDescription('List all entities and records count')
+            ->addOption(
+                self::ZERO_PARAM_NAME,
+                null,
+                InputOption::VALUE_NONE,
+                'Do not display entities with 0 records'
+            )
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
 
+        $zero = $input->getOption(self::ZERO_PARAM_NAME);
+
         $output->writeln(str_repeat('-', self::LINE_HORIZONTAL_LENGTH));
         $output->writeln(sprintf(self::LINE_FORMAT, 'Entity', 'Count'));
         $output->writeln(str_repeat('-', self::LINE_HORIZONTAL_LENGTH));
         // phpcs:enable
-        foreach ($this->getEntityList() as $entity => $count) {
+        foreach ($this->getEntityList($zero) as $entity => $count) {
             $line = sprintf(
                 self::LINE_FORMAT,
                 '<fg=green>' . $this->formatString($entity, self::ENTITY_LENGTH) . '</>',
@@ -51,10 +63,7 @@ class DbList extends Command
         return 0;
     }
 
-    /**
-     * @return string[]
-     */
-    private function getEntityList(): array
+    private function getEntityList(bool $zero): array
     {
         $entities = [];
         $metas = $this->em->getMetadataFactory()->getAllMetadata();
@@ -65,8 +74,12 @@ class DbList extends Command
             $name = $meta->getName();
             $table = $this->em->getClassMetadata($name)->getTableName();
 
-            if (in_array($table, $tables)) {
-                $entities[$name] = $this->countEntityRecords($name);
+            $count = $this->countEntityRecords($name);
+            if (true === $zero && 0 === $count) {
+            } else {
+                if (in_array($table, $tables)) {
+                    $entities[$name] = $count;
+                }
             }
         }
 
